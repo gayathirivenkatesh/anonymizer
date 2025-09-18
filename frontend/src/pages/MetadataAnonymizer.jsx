8721aca1f05f6edd03eb5f15f503a57a03930cde
GAYATHIRI V <166318038+gayathirivenkatesh@users.noreply.github.com>
2025-09-18 15:30:45 +0530

    Update MetadataAnonymizer.jsx

--- frontend/src/pages/MetadataAnonymizer.jsx
+++ frontend/src/pages/MetadataAnonymizer.jsx
@@ -17,7 +17,7 @@ export default function MetadataAnonymizer() {
     formData.append("file", file);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/metadata/process", {
+      const res = await fetch("http://anonymizer-rgr5.onrender.com/api/metadata/process", {
         method: "POST",
         body: formData,
       });
