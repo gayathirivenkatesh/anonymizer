38eca8ba0763e63434b8693727476e86f93085df
GAYATHIRI V <166318038+gayathirivenkatesh@users.noreply.github.com>
2025-09-19 14:09:14 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Smart Anonymizer API")
 # CORS middleware: allow your Vercel frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://anonymizer-seven.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
